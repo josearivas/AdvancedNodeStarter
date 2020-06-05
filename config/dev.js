bf242224ccb63bcaf15c4591d916a857fb00700b
josearivas <rivass.josea@gmail.com>
2020-06-05 13:06:48 -0400

    nuevas crendenciales google oauth2

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    '500681704901-7vqlkta661asf0fcn5686qc2bikb7po8.apps.googleusercontent.com',
+  googleClientSecret: '79VxKGNNECOqmvvYQr8nOfK7',
   mongoURI:
     'mongodb://dev:QIhu1LM5VW4nkWyc@cluster0-shard-00-00-t2utv.mongodb.net:27017,cluster0-shard-00-01-t2utv.mongodb.net:27017,cluster0-shard-00-02-t2utv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
