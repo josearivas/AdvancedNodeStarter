790c74ab719ad70b97ce70976b0b0d0baea183a7
josearivas <rivass.josea@gmail.com>
2020-06-05 13:07:20 -0400

    adicion de upload file

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -47,11 +49,22 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({ file: event.targe.files[0] });
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an image</h5>
+        <input
+          onChange={this.onFileChange.bind(this)}
+          type="file"
+          accept="image/*"
+        />
 
         {this.renderButtons()}
       </form>
